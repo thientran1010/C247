bd98fc265ef0bf9b63ca75596c6a24023a644b46
TravisSLee <travisslee223@gmail.com>
2022-06-13 14:55:12 -0400

    burger code

--- BurgerJoint/Burger.py
+++ BurgerJoint/Burger.py
@@ -3,10 +3,11 @@ import FoodItem
 class Burger(FoodItem):
     
     patty=""
-    
-    def __init__(self, name, price, patty):
+    type=""
+    def __init__(self, name, price, patty, type):
         FoodItem.__init__(self, name, price)
         self.patty=patty
+        self.type=type
 
     def display(self):
         print(self)    
